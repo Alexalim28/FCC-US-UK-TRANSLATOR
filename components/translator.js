f45e8fe53e6900202e3b6d5d27ac0de0a9595d46
Alexalim28 <alexis.ahab@gmail.com>
2022-01-19 23:54:24 +0100

    Added translator logic

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,68 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+  us2Uk(text) {
+    let text2LowerCase = text.toLowerCase();
 
+    for (let word in americanOnly) {
+      text2LowerCase = text2LowerCase.replace(word, americanOnly[word]);
+    }
+
+    for (let title in americanToBritishTitles) {
+      text2LowerCase = text2LowerCase.replace(
+        title,
+        americanToBritishTitles[title]
+      );
+    }
+
+    for (let spelling in americanToBritishSpelling) {
+      text2LowerCase = text2LowerCase.replace(
+        spelling,
+        americanToBritishSpelling[spelling]
+      );
+    }
+
+    text2LowerCase = text2LowerCase.split(" ");
+    text = text
+      .split(" ")
+      .map((word, i) => {
+        if (word.toLowerCase() === text2LowerCase[i]) {
+          return word;
+        } else {
+          if (/[A-Z]/.test(word[0])) {
+            const firstLetter = text2LowerCase[i].charAt(0).toUpperCase();
+            text2LowerCase[i] = firstLetter + text2LowerCase[i].slice(1);
+            return `<span class="highlight">${text2LowerCase[i]}</span>`;
+          }
+          return `<span class="highlight">${text2LowerCase[i]}</span>`;
+        }
+      })
+      .join(" ");
+
+    if (/[0-9]+:[0-9]+/.test(text)) {
+      let [, hour, min] = text.match(/([0-9]+):([0-9]+)/);
+
+      text = text.replace(
+        /[0-9]+:[0-9]+/,
+        `<span class="highlight">${hour}.${min}</span>`
+      );
+      console.log(hour, min);
+    }
+    return text;
+  }
+
+  uk2Us(text) {
+    if (text in americanToBritishTitles) {
+      return `<span class="highlight">${americanToBritishTitles[text]}</span>`;
+    }
+
+    if (/[0-9]+.[0-9]+/.test(text)) {
+      return `<span class="highlight">${text.replace(".", ":")}</span>`;
+    }
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,33 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+
+    if (text === undefined || locale === undefined) {
+      return res.json({ error: "Required field(s) missing" });
+    }
+
+    if (!text) {
+      return res.json({ error: "No text to translate" });
+    }
+
+    if (locale !== "american-to-british" && locale !== "british-to-american") {
+      return res.json({ error: "Invalid value for locale field" });
+    }
+
+    const translation =
+      locale === "american-to-british"
+        ? translator.us2Uk(text)
+        : translator.uk2Us(text);
+
+    if (!translation) {
+      return res.json({ text, translation: "Everything looks good to me!" });
+    }
+    res.json({ text, translation });
+  });
 };
