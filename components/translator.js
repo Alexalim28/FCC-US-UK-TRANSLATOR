cecf1a8a1654fa51cc8b18bd1d0d73c40df0c6c5
Alexalim28 <alexis.ahab@gmail.com>
2022-01-21 15:18:45 +0100

    Fixed bugs in translator functions

--- components/translator.js
+++ components/translator.js
@@ -5,63 +5,190 @@ const britishOnly = require("./british-only.js");
 
 class Translator {
   us2Uk(text) {
-    let text2LowerCase = text.toLowerCase();
+    let newText = text;
 
     for (let word in americanOnly) {
-      text2LowerCase = text2LowerCase.replace(word, americanOnly[word]);
+      let translatedWord;
+      const regex = new RegExp(`(?<=^|\\s)${word}(?=\\.|\\s)`, "i");
+      const searchedWord = text.match(regex) && text.match(regex)[0];
+      if (
+        searchedWord &&
+        searchedWord.charAt(0) === searchedWord.charAt(0).toUpperCase()
+      ) {
+        translatedWord =
+          americanOnly[word].charAt(0).toUpperCase() +
+          americanOnly[word].slice(1);
+      } else {
+        translatedWord = americanOnly[word];
+      }
+      newText = newText.replace(
+        searchedWord,
+        `<span class="highlight">${translatedWord}</span>`
+      );
     }
 
     for (let title in americanToBritishTitles) {
-      text2LowerCase = text2LowerCase.replace(
-        title,
-        americanToBritishTitles[title]
+      let translatedWord;
+      const regex = new RegExp(`(?<=^|\\s)${title}(?=\\.|\\s)`, "i");
+      const searchedWord = text.match(regex) && text.match(regex)[0];
+      if (
+        searchedWord &&
+        searchedWord.charAt(0) === searchedWord.charAt(0).toUpperCase()
+      ) {
+        translatedWord =
+          americanToBritishTitles[title].charAt(0).toUpperCase() +
+          americanToBritishTitles[title].slice(1);
+      } else {
+        translatedWord = americanToBritishTitles[title];
+      }
+      newText = newText.replace(
+        searchedWord,
+        `<span class="highlight">${translatedWord}</span>`
       );
     }
 
     for (let spelling in americanToBritishSpelling) {
-      text2LowerCase = text2LowerCase.replace(
-        spelling,
-        americanToBritishSpelling[spelling]
+      let translatedWord;
+      const regex = new RegExp(`(?<=^|\\s)${spelling}(?=\\.|\\s)`, "i");
+      const searchedWord = text.match(regex) && text.match(regex)[0];
+      if (
+        searchedWord &&
+        searchedWord.charAt(0) === searchedWord.charAt(0).toUpperCase()
+      ) {
+        translatedWord =
+          americanToBritishSpelling[spelling].charAt(0).toUpperCase() +
+          americanToBritishSpelling[spelling].slice(1);
+      } else {
+        translatedWord = americanToBritishSpelling[spelling];
+      }
+      newText = newText.replace(
+        searchedWord,
+        `<span class="highlight">${translatedWord}</span>`
       );
     }
 
-    text2LowerCase = text2LowerCase.split(" ");
-    text = text
-      .split(" ")
-      .map((word, i) => {
-        if (word.toLowerCase() === text2LowerCase[i]) {
-          return word;
-        } else {
-          if (/[A-Z]/.test(word[0])) {
-            const firstLetter = text2LowerCase[i].charAt(0).toUpperCase();
-            text2LowerCase[i] = firstLetter + text2LowerCase[i].slice(1);
-            return `<span class="highlight">${text2LowerCase[i]}</span>`;
-          }
-          return `<span class="highlight">${text2LowerCase[i]}</span>`;
-        }
-      })
-      .join(" ");
+    if (/[0-9]+:[0-9]+/.test(newText)) {
+      let [, hour, min] = newText.match(/([0-9]+):([0-9]+)/);
 
-    if (/[0-9]+:[0-9]+/.test(text)) {
-      let [, hour, min] = text.match(/([0-9]+):([0-9]+)/);
-
-      text = text.replace(
+      newText = newText.replace(
         /[0-9]+:[0-9]+/,
         `<span class="highlight">${hour}.${min}</span>`
       );
-      console.log(hour, min);
     }
-    return text;
+
+    // text2LowerCase = text2LowerCase.split(" ");
+    // let newText = text
+    //   .split(" ")
+    //   .map((word, i) => {
+    //     if (word.toLowerCase() === text2LowerCase[i]) {
+    //       return word;
+    //     } else {
+    //       if (/[A-Z]/.test(word[0])) {
+    //         const firstLetter = text2LowerCase[i].charAt(0).toUpperCase();
+    //         text2LowerCase[i] = firstLetter + text2LowerCase[i].slice(1);
+    //         return `<span class="highlight">${text2LowerCase[i]}</span>`;
+    //       }
+    //       return;
+    //     }
+    //   })
+    //   .join(" ");
+
+    if (newText === text) {
+      return false;
+    }
+    return newText;
   }
 
   uk2Us(text) {
-    if (text in americanToBritishTitles) {
-      return `<span class="highlight">${americanToBritishTitles[text]}</span>`;
+    let newText = text;
+
+    for (let word in britishOnly) {
+      let translatedWord;
+      const regex = new RegExp(`(?<=^|\\s)${word}(?=\\.|\\s)`, "i");
+      const searchedWord = text.match(regex) && text.match(regex)[0];
+      if (
+        searchedWord &&
+        searchedWord.charAt(0) === searchedWord.charAt(0).toUpperCase()
+      ) {
+        translatedWord =
+          britishOnly[word].charAt(0).toUpperCase() +
+          britishOnly[word].slice(1);
+      } else {
+        translatedWord = britishOnly[word];
+      }
+      newText = newText.replace(
+        searchedWord,
+        `<span class="highlight">${translatedWord}</span>`
+      );
     }
 
-    if (/[0-9]+.[0-9]+/.test(text)) {
-      return `<span class="highlight">${text.replace(".", ":")}</span>`;
+    Object.entries(americanToBritishTitles).forEach((title) => {
+      let translatedWord;
+      const regex = new RegExp(`(?<=^|\\s)${title[1]}(?=\\.|\\s)`, "i");
+      const searchedWord = text.match(regex) && text.match(regex)[0];
+      if (
+        searchedWord &&
+        searchedWord.charAt(0) === searchedWord.charAt(0).toUpperCase()
+      ) {
+        translatedWord = title[0].charAt(0).toUpperCase() + title[0].slice(1);
+      } else {
+        translatedWord = title[0];
+      }
+      newText = newText.replace(
+        searchedWord,
+        `<span class="highlight">${translatedWord}</span>`
+      );
+    });
+
+    Object.entries(americanToBritishSpelling).forEach((spelling) => {
+      let translatedWord;
+      const regex = new RegExp(`(?<=^|\\s)${spelling[1]}(?=\\.|\\s)`, "i");
+      const searchedWord = text.match(regex) && text.match(regex)[0];
+      if (
+        searchedWord &&
+        searchedWord.charAt(0) === searchedWord.charAt(0).toUpperCase()
+      ) {
+        translatedWord =
+          spelling[0].charAt(0).toUpperCase() + spelling[0].slice(1);
+      } else {
+        translatedWord = spelling[0];
+      }
+      newText = newText.replace(
+        searchedWord,
+        `<span class="highlight">${translatedWord}</span>`
+      );
+    });
+
+    if (/[0-9]+:[0-9]+/.test(newText)) {
+      let [, hour, min] = newText.match(/([0-9]+):([0-9]+)/);
+
+      newText = newText.replace(
+        /[0-9]+:[0-9]+/,
+        `<span class="highlight">${hour}.${min}</span>`
+      );
     }
+
+    // text2LowerCase = text2LowerCase.split(" ");
+    // let newText = text
+    //   .split(" ")
+    //   .map((word, i) => {
+    //     if (word.toLowerCase() === text2LowerCase[i]) {
+    //       return word;
+    //     } else {
+    //       if (/[A-Z]/.test(word[0])) {
+    //         const firstLetter = text2LowerCase[i].charAt(0).toUpperCase();
+    //         text2LowerCase[i] = firstLetter + text2LowerCase[i].slice(1);
+    //         return `<span class="highlight">${text2LowerCase[i]}</span>`;
+    //       }
+    //       return `<span class="highlight">${text2LowerCase[i]}</span>`;
+    //     }
+    //   })
+    //   .join(" ");
+
+    if (newText === text) {
+      return false;
+    }
+    return newText;
   }
 }
 
